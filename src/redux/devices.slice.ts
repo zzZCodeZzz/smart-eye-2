bae032c64e34e39be35c1d825c94d042f6e93ae4
manu <zach@devconsultberlin.de>
2020-03-23 23:07:03 +0100

    changed type of deviceState

--- src/redux/devices.slice.ts
+++ src/redux/devices.slice.ts
@@ -1,21 +1,33 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 
 type DevicesState = {
-    activeDevice: string;
-    devices: any[];
+    activeDevice: string | null;
+    devices: {
+        [device_id: string]: any
+    };
 }
 
 const initialState: DevicesState = {
-    activeDevice: "device1",
-    devices: []
+    activeDevice: null,
+    devices: {}
 };
 
 const devicesSlice = createSlice({
     name: "devices",
     initialState,
     reducers: {
-        onDevicesReceived(state, action: PayloadAction<any>) {
-            state.devices = action.payload;
+        onDevicesReceived(state, action: PayloadAction<any[]>) {
+            state.devices = action.payload
+                .map(device => {
+                    if (!device.serial_number || device.serial_number.replace(/[^0-9SNC]/g, "").length) {
+                        return {...device, serial_number: "00000"}
+                    }
+                    return device;
+                }).reduce((acc, curr) => ({...acc, [curr.device_id]: curr}), {});
+
+            if (!state.activeDevice) {
+                state.activeDevice = action.payload[0] ? action.payload[0].device_id : null;
+            }
         }
     }
 });
