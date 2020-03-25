38b90f6eb26de2c0baed418867bd2ab614f1309e
manu <zach@devconsultberlin.de>
2020-03-25 05:20:27 +0100

    added dropdown for active device in app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter, Link, Route, Switch as RouterSwitch, useRouteMatch} from "react-router-dom";
 // import Settings from "./settings";
-import {AppBar, CssBaseline, Tab, Tabs, ThemeProvider, Toolbar} from "@material-ui/core";
+import {
+    AppBar,
+    CssBaseline,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    Tab,
+    Tabs,
+    ThemeProvider,
+    Toolbar
+} from "@material-ui/core";
 import Settings from "./components/screens/settings";
-import {Provider, useSelector} from "react-redux";
+import {Provider, useDispatch, useSelector} from "react-redux";
 import store from "./redux/store";
 import {theme} from "./components/ui/layout/theme";
 import MainContainer from "./components/ui/layout/mainContainer";
@@ -13,6 +23,8 @@ import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import {RootState} from "./redux/rootReducer";
 import {Measurement} from "./components/screens/measurement";
+import {setActiveDevice} from './redux/device/radEyeDevicesSlice';
+import {Bluetooth, Flare} from "@material-ui/icons";
 
 export const useTabsWithRouter = (routes: string | string[], defaultRoute: string) => {
     const match = useRouteMatch(routes);
@@ -34,20 +46,43 @@ i18n.use(initReactI18next)
 
 function App() {
 
-    const {tabValue} = useTabsWithRouter(['/measurement', '/log', '/settings'], '/settings');
-
     useConfigureAndConnectMqttClient();
 
-    useSelector((state: RootState) => state.app.dictionary.ready);
+    // triggers rerender when dict is ready
+    const {devices} = useSelector((state: RootState) => ({
+        dictReady: state.app.dictionary.ready,
+        devices: state.radEyeDevices
+    }));
+
+    const dispatch = useDispatch();
+
+    const {tabValue} = useTabsWithRouter(['/measurement', '/log', '/settings'], '/settings');
 
     return (
         <div className="App">
-            <AppBar position="static">
+            {/*Todo sry inline style*/}
+            <AppBar position="static" style={{display: "flex", flexDirection: "row"}}>
                 <Tabs value={tabValue} aria-label="simple tabs example">
                     <Tab label="measurement" value={"/measurement"} component={Link} to={"/measurement"}/>
                     <Tab label="log" value={"/log"} component={Link} to={"/log"}/>
                     <Tab label="settings" value={"/settings"} component={Link} to={"/settings"}/>
                 </Tabs>
+                <FormControl>
+                    <InputLabel htmlFor={"deviceDropDown"}>{"Active Device"}</InputLabel>
+                    <Select
+                        inputProps={{name: "deviceDropDown", id: "deviceDropDown"}}
+                        value={devices.activeDevice}
+                        onChange={(event: React.ChangeEvent<any>) => dispatch(setActiveDevice(event.target.value))}
+                    >
+                        {devices.devices && Object.values(devices.devices).map(device =>
+                            <MenuItem
+                                key={device.device_id}
+                                value={device.device_id}>
+                                {device.device_id}
+                                {device.connection_type === "BLE" ? <Bluetooth/> : <Flare/>}
+                            </MenuItem>)}
+                    </Select>
+                </FormControl>
             </AppBar>
             <RouterSwitch>
                 <Route path={"/measurement"}>
@@ -69,10 +104,10 @@ function App() {
     );
 }
 
-const AppWithRouter = () => (
+const AppWithContext = () => (
     <Provider store={store}>
-        <ThemeProvider theme={theme} >
-            <CssBaseline />
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
             <BrowserRouter>
                 <App/>
             </BrowserRouter>
@@ -80,4 +115,4 @@ const AppWithRouter = () => (
     </Provider>);
 
 
-export default AppWithRouter;
+export default AppWithContext;
