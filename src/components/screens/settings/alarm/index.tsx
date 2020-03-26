5df354da4cfce0dd8b3a9fe45fb1082e7127f748
Joao da Mata <damata@mediaworx.de>
2020-03-26 12:24:07 +0100

    alarm screen

--- src/components/screens/settings/alarm/index.tsx
+++ src/components/screens/settings/alarm/index.tsx
@@ -4,6 +4,7 @@ import {createStyles, Grid, Typography} from "@material-ui/core";
 import AntTextField from "../../../ui/inputs/text";
 import AntSwitch from "../../../ui/inputs/switch";
 import AntSlider from "../../../ui/inputs/slider";
+import AntPaper from "../../../ui/surfaces/paper";
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -13,12 +14,12 @@ const useStyles = makeStyles((theme: Theme) =>
             padding: theme.spacing(1),
         },
         label: {
+            fontWeight: 500,
             textAlign: "right",
             [theme.breakpoints.down("md")]: {
-                textAlign: "center"
-            },
-            [theme.breakpoints.only("xs")]: {
-                textAlign: "left"
+                textAlign: "center",
+                paddingTop: `${theme.spacing(3)}px!important`,
+                paddingBottom: `${theme.spacing(0)}px!important`
             }
         },
         paper: {
@@ -29,8 +30,8 @@ const useStyles = makeStyles((theme: Theme) =>
             padding: 0,
             margin: 0
         },
-        emptySpace: {
-            [theme.breakpoints.down("md")]: {
+        placeHolder: {
+            [theme.breakpoints.down("sm")]: {
                 display: "none"
             },
         }
@@ -43,117 +44,128 @@ const AlarmSettings = () => {
 
     return (
         <Fragment>
-            <Grid container className={classes.gridContainer} spacing={3} alignItems="center">
-                <Grid item xs={12} className={classes.paperTitle}>
-                    <Typography variant="h6">Raten</Typography>
+            <AntPaper>
+                <Grid container className={classes.gridContainer} spacing={3} alignItems="center">
+                    <Grid item xs={12} className={classes.paperTitle}>
+                        <Typography variant="h6">Raten</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={3} lg={2} className={classes.label}>
+                        <Typography variant="subtitle1">Level</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                        <AntTextField name="Alarm 1"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                        <AntTextField name="Alarm 2"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2} className={classes.placeHolder} />
+                    <Grid item xs={12} sm={6} md={3} lg={2} className={classes.placeHolder} />
+                    <Grid item xs={12} sm={6} md={3} lg={2} className={classes.placeHolder} />
                 </Grid>
-                <Grid item xs={12} sm={12} md={3} lg={2}>
-                    <Typography variant="subtitle1" className={classes.label}>Level</Typography>
+                <Grid container className={classes.gridContainer} spacing={3} alignItems="center">
+                    <Grid item xs={12} sm={12} md={3} lg={2} className={classes.label}>
+                        <Typography variant="subtitle1">Zählrate</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                        <AntTextField name="Alarm 1"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                        <AntTextField name="Alarm 2"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                        <AntTextField name="Sigma *)"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                        <AntTextField name="Sigm min rate"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2}>
+                        <AntSwitch target={"settings"} name="securityAlarm">Sicherheitsalarm</AntSwitch>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntTextField name="Alarm 1"/>
+                <Grid container className={classes.gridContainer} spacing={3} alignItems="center">
+                    <Grid item xs={12} sm={12} md={3} lg={2} className={classes.label}>
+                        <Typography variant="subtitle1">Dosierung</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2} >
+                        <AntTextField name="Alarm 1"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2} >
+                        <AntTextField name="Alarm 2"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} lg={2} className={classes.placeHolder} />
+                    <Grid item xs={12} sm={6} md={3} lg={2} className={classes.placeHolder} />
+                    <Grid item xs={12} sm={6} md={3} lg={2} className={classes.placeHolder} />
+                    <Grid item xs={12} sm={6} md={3} lg={2} className={classes.placeHolder} />
                 </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntTextField name="Alarm 2"/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}/>
-                <Grid item xs={12} sm={6} md={3} lg={2}/>
-                <Grid item xs={12} sm={6} md={3} lg={2}/>
-            </Grid>
-            <Grid container className={classes.gridContainer} spacing={3} alignItems="center">
-                <Grid item xs={12} sm={12} md={3} lg={2}>
-                    <Typography variant="subtitle1" className={classes.label}>Zählrate</Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntTextField name="Alarm 1"/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntTextField name="Alarm 2"/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntTextField name="Sigma *)"/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntTextField name="Sigm min rate"/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntSwitch target={"settings"} name="securityAlarm">Sicherheitsalarm</AntSwitch>
-                </Grid>
-            </Grid>
-            <Grid container className={classes.gridContainer} spacing={3} alignItems="center">
-                <Grid item xs={12} sm={12} md={3} lg={2}>
-                    <Typography variant="subtitle1" className={classes.label}>Dosierung</Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntTextField name="Alarm 1"/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}>
-                    <AntTextField name="Alarm 2"/>
-                </Grid>
-                <Grid item xs={12} sm={6} md={3} lg={2}/>
-                <Grid item xs={12} sm={6} md={3} lg={2}/>
-                <Grid item xs={12} sm={6} md={3} lg={2}/>
-            </Grid>
+            </AntPaper><br />
 
-            <Grid container spacing={3}>
-                <Grid item xs={12} className={classes.paperTitle}>
-                    <Typography variant="h6">Dosis</Typography>
+            <AntPaper>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} className={classes.paperTitle}>
+                        <Typography variant="h6">Dosis</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={2} className={classes.label}>
+                        <Typography variant="subtitle1">Level</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={5}>
+                        <AntTextField name="Alarm 1"/>
+                    </Grid>
+                    <Grid item xs={12} sm={5}>
+                        <AntTextField name="Alarm 2"/>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={4}>
-                    <Typography variant="subtitle1" className={classes.label}>Level</Typography>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntTextField name="Alarm 1"/>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntTextField name="Alarm 2"/>
-                </Grid>
-            </Grid>
+            </AntPaper><br />
 
-            <Grid container spacing={3}>
-                <Grid item xs={12} className={classes.paperTitle}>
-                    <Typography variant="h6">Alarmschwelle</Typography>
+            <AntPaper>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} className={classes.paperTitle}>
+                        <Typography variant="h6">Alarmschwelle</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntSwitch target={"settings"} name="nichtAernderbar"/>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntSwitch target={"settings"} name="nichtAernderbar"/>
-                </Grid>
-            </Grid>
+            </AntPaper><br />
 
-            <Grid container spacing={3}>
-                <Grid item xs={12} className={classes.paperTitle}>
-                    <Typography variant="h6">Energieverhältnis</Typography>
+            <AntPaper>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} className={classes.paperTitle}>
+                        <Typography variant="h6">Energieverhältnis</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntSwitch target={"settings"} name="Aktiv"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntTextField name="Minimale Werte"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntSlider target={"device"} name={"Empfindlichkeit Niederenergie"} max={100} min={0} step={10}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntSlider  target={"device"} name={"Empfindlichkeit Hochenergie"} max={100} min={0} step={10}/>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntSwitch target={"settings"} name="Aktiv"/>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntTextField name="Minimale Werte"/>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntSlider target={"device"} name={"Empfindlichkeit Niederenergie"} max={100} min={0} step={10}/>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntSlider  target={"device"} name={"Empfindlichkeit Hochenergie"} max={100} min={0} step={10}/>
-                </Grid>
-            </Grid>
+            </AntPaper><br />
 
-            <Grid container spacing={3}>
-                <Grid item xs={12} className={classes.paperTitle}>
-                    <Typography variant="h6">Alarmsignalisierung</Typography>
+            <AntPaper>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} className={classes.paperTitle}>
+                        <Typography variant="h6">Alarmsignalisierung</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntSwitch target={"settings"} name="Schallgeber"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntSwitch target={"settings"} name="LED"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntSwitch target={"settings"} name="Vibrator"/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AntTextField name="Alarm haltezeit" disabled/>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntSwitch target={"settings"} name="Schallgeber"/>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntSwitch target={"settings"} name="LED"/>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntSwitch target={"settings"} name="Vibrator"/>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <AntTextField name="Alarm haltezeit" disabled/>
-                </Grid>
-            </Grid>
+            </AntPaper>
         </Fragment>
     );
 };
